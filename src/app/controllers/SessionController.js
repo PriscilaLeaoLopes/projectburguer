ca41ddaaaf89c29906651f475cc38331632c3818
PriscilaLeaoLopes <priaparecidaleao@gmail.com>
2024-04-04 16:06:47 -0300

    correção 04/04/2023

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,39 +1,45 @@
-   import * as Yup from 'yup'
-   import User from '../models/User'
+import * as Yup from 'yup'
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth'
+import User from '../models/User'
 
-   class SessionController {
+class SessionController {
     async store(request, response) {
         const schema = Yup.object().shape({
             email: Yup.string().email().required(),
-            passoword: Yup.string().required(),
+            password: Yup.string().required(),
         })
-     
+
         const userEmailOrPasswordIncorrect = () => {
             return response
-            .status(401)
-            .json({ error: 'Make sure your password or email are correct'})
-        
+                .status(401)
+                .json({ error: 'make sure your password or email are correct' })
         }
-       
-        if (!(await schema.isValid(request.body))) userEmailOrPasswordIncorrect()
-         
+
         const { email, password } = request.body
 
+        if (!(await schema.isValid(request.body))) {
+            return userEmailOrPasswordIncorrect()
+        }
         const user = await User.findOne({
             where: { email },
         })
 
-        if(!user) userEmailOrPasswordIncorrect()
-
-       if(!(await user.checkPassword(password))) userEmailOrPasswordIncorrect()
+        if (!user)
+            return userEmailOrPasswordIncorrect()
+            if (!(await user.checkPassword(password)))
+            return userEmailOrPasswordIncorrect()
 
         return response.json({
             id: user.id,
             email,
             name: user.name,
             admin: user.admin,
+            token: jwt.sign({id: user.id}, authConfig.secret, {
+                expiresIn: authConfig.expiresIn,
+            }),
         })
     }
-   }
+}
 
 export default new SessionController()
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,26 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
 
 
 
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
+import ProductController from './app/controllers/ProductController'
+
+
+import authMiddleware from './app/middlewares/auth'
+const upload = multer(multerConfig)
 
 const routes = new Router()
 
 routes.post('/users', UserController.store) 
 
 routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddleware)
+
+routes.post('/products', upload.single('file'), ProductController.store)
+routes.get('/products', ProductController.index)
     
 export default routes
